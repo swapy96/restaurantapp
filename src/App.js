0a22a6a39d3679029131b3022467878dd04b5ad5
swapy96 <s.swapnil80@gmail.com>
2023-04-04 11:42:46 -0700

    App.js test style changed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="w-screen h-screen flex items-center justify-center text-blue-600">App</div>;
+  return <div className="w-screen h-auto flex flex-col"></div>;
 }
 
 export default App;
