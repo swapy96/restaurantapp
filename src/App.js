49ebd369ae47c0e105ee0d39569e9bd731e74a9b
swapy96 <s.swapnil80@gmail.com>
2023-04-04 23:49:18 -0700

    set up routes and basic animation to avatar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import { Header } from "./components";
+import { Route, Routes } from "react-router-dom";
+import { Header, MainContainer, CreateContainer } from "./components";
+import { AnimatePresence } from "framer-motion";
 
 function App() {
   return (
-    <div className="w-screen h-auto flex flex-col bg-primary">
-      <Header />
-      <main className="mt-24 p-8 w-full">
-        <Routes>
-          <Route path="/*" element={<MainContainer />} />
-          <Route path="/createItem" element={<CreateContainer />} />
-        </Routes>
-      </main>
-    </div>
+    <AnimatePresence>
+      <div className="w-screen h-auto flex flex-col bg-primary">
+        <Header />
+        <main className="mt-24 p-8 w-full">
+          <Routes>
+            <Route path="/*" element={<MainContainer />} />
+            <Route path="/createItem" element={<CreateContainer />} />
+          </Routes>
+        </main>
+      </div>
+    </AnimatePresence>
   );
 }
 
