e15b265ecff876a4e8a9014cc8c19574af1bcf8b
swapy96 <s.swapnil80@gmail.com>
2023-04-09 23:25:13 -0700

    complete food section - todo some style issue card

--- src/components/RowContainer.jsx
+++ src/components/RowContainer.jsx
@@ -8,22 +8,25 @@ const RowContainer = ({ flag, data, scrollValue }) => {
   }, [scrollValue]);
 
   return (
-    <div ref={rowContainer} className={`flex items-center gap-3 w-full my-12 scroll-smooth ${flag ? "overflow-x-scroll scrollbar-none " : "overflow-x-hidden flex-wrap"}`}>
+    <div ref={rowContainer} className={`flex items-center gap-3 w-full my-12 scroll-smooth ${flag ? "overflow-x-scroll scrollbar-none " : "overflow-x-hidden flex-wrap justify-center"}`}>
       {data &&
         data.map((item) => (
-          <div key={item.id} className="w-300 min-w-[300px] md:w-340 md:min-w-[340px] h-auto bg-cardOverlay rounded-lg p-2 my-12 backdrop-blur-lg hover:drop-shadow-lg">
+          <div key={item.id} className="w-275 h-[175px] min-w-[275px] md:w-300 md:min-w-[300px]  bg-cardOverlay rounded-lg py-2 px-4  my-12 backdrop-blur-lg hover:drop-shadow-lg flex flex-col items-center justify-evenly relative">
             <div className="w-full flex items-center justify-between">
-              <motion.img whileHover={{ scale: 1.2 }} src="https://firebasestorage.googleapis.com/v0/b/restaurantapp-2b979.appspot.com/o/Images%2F1680942120087-c4.png?alt=media&token=97037acb-437e-4570-96bc-86087b01f05d" alt="food item" className="w-40 -mt-8 drop-shadow-2xl" />
+              <motion.div whileHover={{ scale: 1.2 }} className="w-40 h-40 -mt-8 drop-shadow-2xl">
+                <img src={item?.imageURL} alt="food item" className="w-full h-full object-contain" />
+              </motion.div>
               <motion.div whileTap={{ scale: 0.75 }} className="w-8 h-8 rounded-full bg-red-600 flex justify-center cursor-pointer hover:shadow-md items-center">
                 <MdShoppingBasket className="text-white" />
               </motion.div>
             </div>
             <div className="w-full flex flex-col items-end justify-end">
-              <p className="text-textcolor font-semibold text-base md:text-lg">Chicken fried</p>
-              <p className="mt-1 text-sm text-gray-500 ">Calories 45</p>
+              <p className="text-textcolor font-semibold text-base md:text-lg">{item?.title}</p>
+              <p className="mt-1 text-sm text-gray-500 ">{item?.calories}</p>
               <div className="flex items-center gap-8">
                 <p className="text-lg text-headingColor font-semibold">
-                  <span className="text-sm text-red-500">$</span>5.25
+                  <span className="text-sm text-red-500">$</span>
+                  {item?.price}
                 </p>
               </div>
             </div>
