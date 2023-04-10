c4417ade3552c4e0f307248afc4f472c93b839fd
swapy96 <s.swapnil80@gmail.com>
2023-04-10 01:22:25 -0700

    displays filtered data on food category and also added food not available in case there unavailability of food item

--- src/components/RowContainer.jsx
+++ src/components/RowContainer.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect } from "react";
 import { MdShoppingBasket } from "react-icons/md";
 import { motion } from "framer-motion";
+import NotFound from "../img/NotFound.svg";
 const RowContainer = ({ flag, data, scrollValue }) => {
   const rowContainer = useRef();
   useEffect(() => {
@@ -9,7 +10,7 @@ const RowContainer = ({ flag, data, scrollValue }) => {
 
   return (
     <div ref={rowContainer} className={`flex items-center gap-3 w-full my-12 scroll-smooth ${flag ? "overflow-x-scroll scrollbar-none " : "overflow-x-hidden flex-wrap justify-center"}`}>
-      {data &&
+      {data && data.length > 0 ? (
         data.map((item) => (
           <div key={item.id} className="w-275 h-[175px] min-w-[275px] md:w-300 md:min-w-[300px]  bg-cardOverlay rounded-lg py-2 px-4  my-12 backdrop-blur-lg hover:drop-shadow-lg flex flex-col items-center justify-evenly relative">
             <div className="w-full flex items-center justify-between">
@@ -31,7 +32,13 @@ const RowContainer = ({ flag, data, scrollValue }) => {
               </div>
             </div>
           </div>
-        ))}
+        ))
+      ) : (
+        <div className="w-full flex flex-col items-center justify-center">
+          <img src={NotFound} alt="Not found" className="h-340" />
+          <p className="text-xl text-headingColor font-semibold my-2">Sorry!, Dish Not Available.</p>
+        </div>
+      )}
     </div>
   );
 };
